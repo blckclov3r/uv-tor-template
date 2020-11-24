04e2e4353cdf5ef4e996ddf66f956e4fb56aceca
blckclov3r <blckclov3r@gmail.com>
2020-11-23 23:43:49 -0800

    initial commit

---
+++ js/main.js
@@ -0,0 +1,44 @@
+// JavaScript source code
+
+$(document).ready(function () {
+
+    $("#mButton").click(function () {
+ 
+
+        html2canvas(document.getElementById("mContainer")).then(function (canvas) {
+            var link = document.createElement("a");
+            document.body.appendChild(link);
+            link.download = "tor-page1.jpg";
+            link.href = canvas.toDataURL();
+            link.target = '_blank';
+            link.click();
+        }); 
+
+        html2canvas(document.getElementById("mContainer")).then(function (canvas) {
+            var link = document.createElement("a");
+            document.body.appendChild(link);
+            link.download = "tor-page2.jpg";
+            link.href = canvas.toDataURL();
+            link.target = '_blank';
+            link.click();
+        }); 
+
+
+    
+         
+       /*  var container = document.getElementById("target");; 
+        html2canvas(container, { allowTaint: true }).then(function (canvas) {
+
+            var link = document.createElement("a");
+            document.body.appendChild(link);
+            link.download = "html_image.png";
+            link.href = canvas.toDataURL("image/png");
+            link.target = '_blank';
+            link.click();
+        }); */
+
+            
+    });
+
+   
+});
